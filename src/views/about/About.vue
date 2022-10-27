480533474787d684081c25751e7c607020daf9fc
hsiaokang <hsiaokang@163.com>
2022-10-27 11:42:19 +0800

    test husky

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -3,14 +3,14 @@ import { useUserStore } from '@/stores/user'
 import { useRoute } from 'vue-router'
 const userStore = useUserStore()
 const route = useRoute()
-console.log();
+console.log(route)
 
 defineProps({
     msg: String,
 })
 
 const login = () => {
-    userStore.login();
+    userStore.login()
 }
 const logout = () => {
     userStore.logout()
@@ -23,7 +23,7 @@ const logout = () => {
         <button v-if="userStore.name" @click="logout">logout</button>
         <button v-else @click="login">login</button>
     </div>
-    <p>add some text </p>
+    <p>add some text</p>
 </template>
 
 <style scoped lang="scss">
